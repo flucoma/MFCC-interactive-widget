0a9e1ba8f4650fb6c27080e0985c33dd0aa4cebb
Ted Moore <ted@tedmooremusic.com>
2022-02-09 17:19:23 -0500

    dct & idct working; cross interactivity working

--- MultiSlider.js
+++ MultiSlider.js
@@ -1,6 +1,7 @@
 class MultiSlider {
   
-  constructor(x,y,w,h,min,max,ref){
+  constructor(x,y,w,h,min,max,ref,showNumbers){
+    this.showNumbers = showNumbers;
     this.x = x;
     this.y = y;
     this.w = w;
@@ -9,6 +10,7 @@ class MultiSlider {
     this.min = min;
     this.max = max;
     this.values = [0];
+    this.action = null;
   }
 
   getBarWidth(){
@@ -33,23 +35,38 @@ class MultiSlider {
       let barx = bar_gap + (bar_skip*i);
       let bary = map(this.reference,this.min,this.max,this.h,0);
       let bartop = map(this.values[i],this.min,this.max,this.h,0);
+      // TODO: constrain bartop to within window.
       fill(0);
       rectMode(CORNERS);
       rect(this.x + barx,this.y + bary, this.x + barx + bar_width, this.y + bartop);
-      fill(255);
-      textAlign(CENTER);
-      text(round(this.values[i],2),this.x + barx, (this.y + bary) - 15, bar_width);
+      if(this.showNumbers){
+        fill(255);
+        textAlign(CENTER);
+        text(round(this.values[i],2),this.x + barx, (this.y + bary) - 15, bar_width);
+      }
     }
+  }
+
+  setAction(func){
+    this.action = func;
   }
   
   value(vals){
     this.values = vals;
   }
 
-  valueFromMouse(x,y){
+  valueAction(vals){
+    this.values = vals;
+    this.action();
+  }
+
+  setValueFromMouse(x,y){
     let slider_y = map(y,0,this.h,this.max,this.min);
     let index = this.indexFromMouse(x);
-    this.values[index] = slider_y;
+    if(index != null){
+      this.values[index] = slider_y;
+      this.action();
+    }
   }
 
   indexFromMouse(x){
@@ -62,17 +79,18 @@ class MultiSlider {
         return i;
       }
     }
+    return null;
   }
 
   mousePressed(x,y){
     if(x > this.x && x < (this.x + this.w) && y > this.y && y < (this.y + this.h)){
-      this.valueFromMouse(x - this.x,y - this.y);
+      this.setValueFromMouse(x - this.x,y - this.y);
     }
   }
 
   mouseDragged(x,y){
     if(x > this.x && x < (this.x + this.w) && y > this.y && y < (this.y + this.h)){
-      this.valueFromMouse(x - this.x,y - this.y);
+      this.setValueFromMouse(x - this.x,y - this.y);
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,44 @@
 let mel_ms;
 let mfcc_ms;
 let margin = 10;
+let pi = 3.14159265359;
 
 function setup() {
-  createCanvas(1000, 600);
+  createCanvas(1600, 600);
   let ms_width = (width - (margin*3)) / 2;
   let ms_height = (height - (margin * 3)) * 0.7;
-  mel_ms = new MultiSlider(margin,margin,ms_width,ms_height,0,100,0);
-  mfcc_ms = new MultiSlider((margin*2) + ms_width,margin,ms_width,ms_height,-80,80,0);
-  
-  mel_ms.value([10,40,90,10,80,40,100])
-  mfcc_ms.value([-60,40,20,15,-49,-70,0,10])
+  mel_ms = new MultiSlider(margin,margin,ms_width,ms_height,0,100,0,false);
+  mfcc_ms = new MultiSlider((margin*2) + ms_width,margin,ms_width,ms_height,-80,80,0,true);
+
+  let mels = [];
+  for(let i = 0; i < 40; i++){
+    mels[i] = 0;
+  }
+
+  let mfccs = [];
+  for(let i = 0; i < 13; i++){
+    mfccs[i] = 0;
+  }
+
+  mel_ms.value(mels)
+  mfcc_ms.value(mfccs);
+
+  mel_ms.setAction(() => {
+    // print('function called')
+    mfcc_ms.value(dct(mel_ms.values,mfcc_ms.values.length));
+  })
+
+  mfcc_ms.setAction(() => {
+    let longenough = [];
+    for(let i = 0; i < mel_ms.values.length; i++){
+      if(i < mfcc_ms.values.length){
+        longenough[i] = mfcc_ms.values[i]
+      } else {
+        longenough[i] = 0;
+      } 
+    }
+    mel_ms.value(idct(longenough,longenough.length))
+  })
 }
 
 function draw() {
@@ -32,5 +60,45 @@ function mouseDragged(){
 }
 
 function dct(array,nCoeffs){
-  
+  let out = [];
+
+  for(let k = 0; k < nCoeffs; k++){
+    let v = [];
+    for(let n = 0; n < array.length; n++){
+      let xn = array[n];
+      let x = (((2*n)+1)*pi*k) / (2*array.length);
+      let c = cos(x) * xn;
+      v[n] = c;
+    }
+    let sum = v.reduce((total,val,idx,arr) => total + val);
+    let scale;
+    if(k == 0) {
+      scale = Math.sqrt(1/(4 * array.length));
+    } else {
+      scale = Math.sqrt(1/(2 * array.length));
+    }
+    out[k] = sum * 2 * scale;
+  }  
+
+  return out;
+}
+
+function idct(values,n_coeffs){
+  let out = [];
+
+  for(let k = 0; k < n_coeffs; k++){
+    let scale = Math.sqrt(2/values.length);
+    let v = [];
+    for(let n = 1; n < values.length; n++){
+      let xn = values[n];
+      let x = (((2 * k) + 1) * pi * n) / (2 * values.length);
+      let c = cos(x) * xn;
+      v[n] = c
+    };
+    let sum = v.reduce((total,val,idx,arr) => total + val);
+    let off = values[0] / Math.sqrt(values.length);
+    out[k] = off + (sum * scale)
+  }
+
+  return out;
 }
